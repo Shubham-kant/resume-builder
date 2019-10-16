f9e37733eeabd5ceac0e8ab482be6013ec9ccc5a
Shubham-Kant <pro.shubham1999@gmail.com>
2019-10-16 19:22:27 +0530

    first commit:setup express

---
+++ index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const port=8000;
+const app=express();
+
+app.listen(port,function(err){
+    if(err){
+        consolr.log(`error is : ${err}`);
+        return;
+    }
+    console.log(`port is running on port no.: ${port}`);
+
+
+
+
+})
