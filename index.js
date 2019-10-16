76d7e7a5d247451d26bd5a8c92e4d91a69afe7eb
Shubham-Kant <pro.shubham1999@gmail.com>
2019-10-16 19:33:36 +0530

    added router for home

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+app.get('/',require('./routes'));
+
 app.listen(port,function(err){
     if(err){
         consolr.log(`error is : ${err}`);
