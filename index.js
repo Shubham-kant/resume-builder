352d592d271c2796bd8b1e284b9179b9b369d2d0
Shubham-Kant <pro.shubham1999@gmail.com>
2019-10-16 19:48:42 +0530

    set up view engine

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port=8000;
 const app=express();
 
 app.get('/',require('./routes'));
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
