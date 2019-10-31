bb3bbb1b1066d6b028805ed81513be836d13e25f
Shubham-Kant <pro.shubham1999@gmail.com>
2019-11-01 02:43:00 +0530

    removes content with remove button

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,12 @@
 console.log('ss');
+var i=1;
+var j=1;
+var k=1;
+
 $('#ed-add-btn').click(function(){
     
-    $("#education").append(`<div>
-    <button class="remove-btn">X</button>
+    $("#education").append(`<div id="ed-${i}">
+    <button class="remove-btn" id="ed-remove-btn">X</button>
     <div class="info">
         <input type="text" name="degree" placeholder="degree" required>
         <input type="text" name="college" placeholder="college or university name" required>
@@ -15,14 +19,15 @@ $('#ed-add-btn').click(function(){
     </div>
 </div>`);
 
+    i++;
 });
 $('#exp-add-btn').click(function(){
     // var div=$('.info');
     // $('.common-sec').append(div);
     //div = document.createElement('div');
     //$(div).addClass("info").html('');
-    $("#exp").append(`<div >
-    <button class="remove-btn">X</button>
+    $("#exp").append(`<div id="exp-${j}">
+    <button class="remove-btn" id="exp-remove-btn">X</button>
     <div class="info">
         <input type="text" name="name" placeholder="company name" required>
         <textarea name="description" placeholder="describe what you have done in the company?" required rows="10"></textarea>
@@ -35,21 +40,43 @@ $('#exp-add-btn').click(function(){
         <input type="date" name="end" id="end-date" value="2019-01-01" required>
     </div>
 </div>`);
-
+    
+    j++;
 });
 $('#project-add-btn').click(function(){
     // var div=$('.info');
     // $('.common-sec').append(div);
     //div = document.createElement('div');
     //$(div).addClass("info").html('');
-    $("#project").append(`<div>
-    <button class="remove-btn">X</button>
-    <div class="info">
+    $("#project").append(`<div id="project-${k}">
+    <button class="remove-btn" id="project-remove-btn">X</button>
+    <div class="info" >
             <input type="text" name="project-name" placeholder="project's name" required>
             
             <textarea name="project-description" placeholder="describe your project" required rows="10"></textarea>
 
     </div>
 </div>`);
+    
+    k++;
 
+});
+
+
+// $('.remove-btn').click(function(){
+
+//     // var child = document.getElementById(k);
+//     // var parent = document.getElementById(project);
+//     // parent.removeChild(child);
+
+//     // $(`#project-${z}`).remove();
+
+
+    
+
+// });
+
+$(document).on('click','.remove-btn',function(e){
+    //alert('hello');
+    $(this).parent('div').remove();
 });
