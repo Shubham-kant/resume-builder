d7a93ac198f20c63efa90d58f92e0aba3fc0c7cb
Shubham-Kant <pro.shubham1999@gmail.com>
2020-04-25 18:07:16 +0530

    changes done for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
-const port=8000;
+const port=process.env.PORT || 8000;
 const app=express();
 
 const bodyParser=require('body-parser');
